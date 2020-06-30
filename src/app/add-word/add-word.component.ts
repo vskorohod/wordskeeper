78b74b49f0eaab7d545b1a9e979f5e02202ca3a3
Vladimir Skorokhod <vaskorohod@gmail.com>
2020-06-30 10:55:35 +0200

    Add translate function for foreign word

--- src/app/add-word/add-word.component.ts
+++ src/app/add-word/add-word.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Word} from '../word.model';
 import {WordService} from '../word.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-add-word',
@@ -9,12 +10,16 @@ import {WordService} from '../word.service';
   styleUrls: ['./add-word.component.scss']
 })
 
-export class AddWordComponent implements OnInit {
+export class AddWordComponent implements OnInit, OnDestroy {
   addWordForm: FormGroup;
   isSending = false;
+  translatedWordSub: Subscription;
   constructor(private wordService: WordService) {}
 
   ngOnInit() {
+    this.translatedWordSub = this.wordService.translatedWord.subscribe(translatedWord => {
+      this.addWordForm.patchValue({nativeWord: translatedWord});
+    });
     this.addWordForm = new FormGroup({
       foreignWord: new FormControl(null, Validators.required),
       nativeWord: new FormControl(null, Validators.required),
@@ -24,5 +29,11 @@ export class AddWordComponent implements OnInit {
   onAddWord({foreignWord, nativeWord, comment}: {foreignWord: string, nativeWord: string, comment: string}) {
     const newWord: Word = {foreignWord, nativeWord, comment, createDate: new Date()};
     this.wordService.addWords(newWord);
+  }
+  onTranslateWord(foreignWord: string) {
+    this.wordService.translateWord(foreignWord);
+  }
+  ngOnDestroy() {
+    this.translatedWordSub.unsubscribe();
   }
 }
